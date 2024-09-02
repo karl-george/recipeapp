e0132704747fbc5bd1eb898fc78a77555ac1eb95
Karl George <karlgeorge87@gmail.com>
2024-09-02 14:33:34 +0200

    Style food card title and info

--- components/FoodCard.tsx
+++ components/FoodCard.tsx
@@ -14,7 +14,8 @@ type FoodCardProps = {
 
 const FoodCard = ({ label, image, time, calories }: FoodCardProps) => {
   return (
-    <View className='mb-3'>
+    <View className='mb-4'>
+      {/* //todo add favourite icon */}
       <View className='h-[220px] w-[182px]'>
         <Image
           src={image.url}
@@ -25,12 +26,19 @@ const FoodCard = ({ label, image, time, calories }: FoodCardProps) => {
 
       <View>
         {/* Title */}
-        <Text className='w-[182px] font-semibold text-lg'>{label}</Text>
-        <View>
+        <Text
+          className='w-[182px] font-bold text-base'
+          numberOfLines={1}
+          ellipsizeMode='tail'
+        >
+          {label}
+        </Text>
+        {/* // todo add time and calories icons */}
+        <View className='flex-row items-center space-x-4'>
           {/* Time */}
-          <Text className='text-faded'>{time}</Text>
+          <Text className='text-faded'>{time}min</Text>
           {/* Calories */}
-          <Text className='text-faded'>{calories}</Text>
+          <Text className='text-faded'>{calories.toFixed(0)} cal</Text>
         </View>
       </View>
     </View>
