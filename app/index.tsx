5ed865eecfd32f8a6ee87cf24670330eebb5ed1e
Karl George <karlgeorge87@gmail.com>
2024-09-02 11:35:18 +0200

    Add spacing

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,26 @@
 import CategoryList from '@/components/CategoryList';
+import { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+  const [categorySelected, setCategorySelected] = useState('Breakfast');
+
   return (
     <View className='px-4 bg-bgFaded'>
       {/* Search Bar */}
       {/*// todo  */}
       {/* Categories */}
-      <View className='flex-row items-center justify-between'>
+      <View className='flex-row items-center justify-between mb-2'>
         <Text className='text-xl font-bold'>Categories</Text>
         <TouchableOpacity onPress={() => {}}>
           <Text className='font-semibold text-accent'>See all</Text>
         </TouchableOpacity>
       </View>
 
-      <CategoryList />
+      <CategoryList
+        categorySelected={categorySelected}
+        setCategorySelected={setCategorySelected}
+      />
       {/* Recommended */}
       {/* // todo */}
     </View>
