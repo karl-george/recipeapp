152eefb45a1fe19289f6228a1b3a1f5fc9ad8949
Karl George <karlgeorge87@gmail.com>
2024-09-02 11:35:40 +0200

    Add category styling

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,24 +1,39 @@
-import {
-  View,
-  Text,
-  FlatList,
-  ScrollView,
-  TouchableOpacity,
-} from 'react-native';
-import React from 'react';
 import { categories } from '@/constants/data';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
-const CategoryList = () => {
+const CategoryList = ({ categorySelected, setCategorySelected }: any) => {
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
-      contentContainerStyle={{ flexGrow: 1, gap: 6 }}
+      contentContainerStyle={{
+        gap: 8,
+      }}
     >
       {categories.map((category, i) => (
-        <View key={i} className='px-4 py-2 border-2 rounded-md border-faded'>
-          <TouchableOpacity>
-            <Text className={`text-faded`}>{category}</Text>
+        <View key={i}>
+          <TouchableOpacity onPress={() => setCategorySelected(category)}>
+            {categorySelected === category ? (
+              <LinearGradient
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                colors={['#EA7A22', '#F5A150']}
+                className='items-center px-5 py-3 rounded-md'
+              >
+                <Text className='font-semibold text-white bg-transparent'>
+                  {category}
+                </Text>
+              </LinearGradient>
+            ) : (
+              <TouchableOpacity
+                className='items-center px-5 py-[11px] border rounded-md border-faded'
+                onPress={() => setCategorySelected(category)}
+              >
+                <Text className=' text-faded'>{category}</Text>
+              </TouchableOpacity>
+            )}
           </TouchableOpacity>
         </View>
       ))}
