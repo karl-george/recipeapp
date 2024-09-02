71044e8ac8a7b8029bd7a9f6367d1dd09bf67fe3
Karl George <karlgeorge87@gmail.com>
2024-09-02 14:17:40 +0200

    Add food card

--- components/FoodCard.tsx
+++ components/FoodCard.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
 
-const FoodCard = () => {
+type FoodCardProps = {
+  label: string;
+  image: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  time: number;
+  calories: number;
+};
+
+const FoodCard = ({ label, image, time, calories }: FoodCardProps) => {
   return (
-    <View>
-      <Text>FoodCard</Text>
+    <View className='mb-3'>
+      <View className='h-[220px] w-[182px]'>
+        <Image
+          src={image.url}
+          className='w-full h-full rounded-xl'
+          resizeMode='cover'
+        />
+      </View>
+
+      <View>
+        {/* Title */}
+        <Text className='w-[182px] font-semibold text-lg'>{label}</Text>
+        <View>
+          {/* Time */}
+          <Text className='text-faded'>{time}</Text>
+          {/* Calories */}
+          <Text className='text-faded'>{calories}</Text>
+        </View>
+      </View>
     </View>
   );
 };
