6a789fbf983357f9b8552543172bcd90e64b9872
Karl George <karlgeorge87@gmail.com>
2024-09-04 09:48:47 +0200

    Add icons to recipe info

--- components/FoodCard.tsx
+++ components/FoodCard.tsx
@@ -1,4 +1,5 @@
 import { View, Text, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 
 type FoodCardProps = {
@@ -33,12 +34,19 @@ const FoodCard = ({ label, image, time, calories }: FoodCardProps) => {
         >
           {label}
         </Text>
-        {/* // todo add time and calories icons */}
         <View className='flex-row items-center space-x-4'>
           {/* Time */}
-          <Text className='text-faded'>{time}min</Text>
+          <View className='flex-row items-center space-x-1'>
+            <Ionicons name='time-outline' size={14} color={'#EA7A22'} />
+            <Text className='text-sm text-faded'>{time}min</Text>
+          </View>
           {/* Calories */}
-          <Text className='text-faded'>{calories.toFixed(0)} cal</Text>
+          <View className='flex-row items-center space-x-1'>
+            <Ionicons name='flame' size={14} color={'#EA7A22'} />
+            <Text className='text-sm text-faded'>
+              {calories.toFixed(0)} cal
+            </Text>
+          </View>
         </View>
       </View>
     </View>
