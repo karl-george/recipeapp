91ce9eba31cf9b78cc89b18e8a2710851012eea1
Karl George <karlgeorge87@gmail.com>
2024-09-04 13:46:02 +0200

    Refactor root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,36 @@
-import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack, usePathname, useRouter } from 'expo-router';
+import { useEffect } from 'react';
 
-export default function RootLayout() {
+const InitialLayout = () => {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
     </Stack>
   );
-}
+};
+
+const RootLayoutNav = () => {
+  return <InitialLayout />;
+};
+
+export default RootLayoutNav;
