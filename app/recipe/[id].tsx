f1fa0906dcd87eeab10355038ca665ba81ff1e07
Karl George <karlgeorge87@gmail.com>
2024-09-05 12:58:19 +0200

    Style details page

--- app/recipe/[id].tsx
+++ app/recipe/[id].tsx
@@ -1,25 +1,42 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, ImageBackground } from 'react-native';
 import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import { detailedRecipe } from '@/constants/data';
+import { Ionicons } from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
 
 const RecipeDetails = () => {
   const { id } = useLocalSearchParams();
 
   return (
-    <ScrollView>
+    <ScrollView className='flex-1 w-full'>
+      <StatusBar translucent />
       <View>
-        {/* Image */}
+        <ImageBackground
+          src={detailedRecipe?.image}
+          className='w-full h-[300px]'
+          resizeMode='cover'
+        />
         {/* Back Button */}
         {/* Fave Button */}
       </View>
       {/* Info */}
-      <View>
-        {/* Title */}
-        {/* mins and calories */}
-        {/* Description */}
-        {/* Ingredients */}
-        {/* Directions */}
+      <View className='px-4 mt-4'>
+        <Text className='text-xl font-bold'>{detailedRecipe?.title}</Text>
+        <View className='flex-row space-x-4'>
+          <View className='flex-row items-center space-x-1'>
+            <Ionicons name='time-outline' size={14} color={'#EA7A22'} />
+            <Text className='text-sm text-faded'>
+              {detailedRecipe?.readyInMinutes} min
+            </Text>
+          </View>
+          <View className='flex-row items-center space-x-1'>
+            <Ionicons name='people' size={14} color={'#EA7A22'} />
+            <Text className='text-sm text-faded'>
+              {detailedRecipe?.servings}
+            </Text>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
