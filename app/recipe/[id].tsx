3dfda3de754856d7383c39ce5cc3770626760f6b
Karl George <karlgeorge87@gmail.com>
2024-09-05 13:40:58 +0200

    Style instructions list

--- app/recipe/[id].tsx
+++ app/recipe/[id].tsx
@@ -43,7 +43,7 @@ const RecipeDetails = () => {
         {/* Ingredients */}
         <View className='mt-6'>
           <Text className='mb-3 text-xl font-bold'>Ingredients</Text>
-          {detailedRecipe.extendedIngredients.map((ingredient) => (
+          {detailedRecipe?.extendedIngredients.map((ingredient) => (
             <View className='flex-row items-center mb-4 space-x-2'>
               <Image
                 src={`https://img.spoonacular.com/ingredients_100x100/${ingredient.image}`}
@@ -58,6 +58,17 @@ const RecipeDetails = () => {
                   {ingredient.amount} {ingredient.measures.us.unitShort}
                 </Text>
               </View>
+            </View>
+          ))}
+        </View>
+
+        {/* Instructions */}
+        <View className='mt-4'>
+          <Text className='mb-3 text-xl font-bold'>Instructions</Text>
+          {detailedRecipe?.analyzedInstructions[0].steps.map((step) => (
+            <View className='flex-row mb-4 space-x-2'>
+              <Text className='text-lg'>{step.number}.</Text>
+              <Text className='w-11/12 text-lg'>{step.step}</Text>
             </View>
           ))}
         </View>
