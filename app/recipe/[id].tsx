ab7a6f50e54139bca72f74bdcc88a2a54df715e2
Karl George <karlgeorge87@gmail.com>
2024-09-05 10:46:48 +0200

    Add detailed recipe page

--- app/recipe/[id].tsx
+++ app/recipe/[id].tsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import { detailedRecipe } from '@/constants/data';
 
 const RecipeDetails = () => {
+  const { id } = useLocalSearchParams();
+
   return (
-    <View>
-      <Text>RecipeDetails</Text>
-    </View>
+    <ScrollView>
+      <View>
+        {/* Image */}
+        {/* Back Button */}
+        {/* Fave Button */}
+      </View>
+      {/* Info */}
+      <View>
+        {/* Title */}
+        {/* mins and calories */}
+        {/* Description */}
+        {/* Ingredients */}
+        {/* Directions */}
+      </View>
+    </ScrollView>
   );
 };
 
