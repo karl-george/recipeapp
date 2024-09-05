2175d5025f12549147aea4badcf60b70b3e5c80b
Karl George <karlgeorge87@gmail.com>
2024-09-05 13:26:19 +0200

    Add ingredients

--- app/recipe/[id].tsx
+++ app/recipe/[id].tsx
@@ -1,9 +1,9 @@
-import { View, Text, ScrollView, ImageBackground } from 'react-native';
-import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
 import { detailedRecipe } from '@/constants/data';
 import { Ionicons } from '@expo/vector-icons';
+import { useLocalSearchParams } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Image, ImageBackground, ScrollView, Text, View } from 'react-native';
 
 const RecipeDetails = () => {
   const { id } = useLocalSearchParams();
@@ -20,9 +20,11 @@ const RecipeDetails = () => {
         {/* Back Button */}
         {/* Fave Button */}
       </View>
+
       {/* Info */}
       <View className='px-4 mt-4'>
-        <Text className='text-xl font-bold'>{detailedRecipe?.title}</Text>
+        {/* Title */}
+        <Text className='text-2xl font-bold'>{detailedRecipe?.title}</Text>
         <View className='flex-row space-x-4'>
           <View className='flex-row items-center space-x-1'>
             <Ionicons name='time-outline' size={14} color={'#EA7A22'} />
@@ -36,6 +38,28 @@ const RecipeDetails = () => {
               {detailedRecipe?.servings}
             </Text>
           </View>
+        </View>
+
+        {/* Ingredients */}
+        <View className='mt-6'>
+          <Text className='mb-3 text-xl font-bold'>Ingredients</Text>
+          {detailedRecipe.extendedIngredients.map((ingredient) => (
+            <View className='flex-row items-center mb-4 space-x-2'>
+              <Image
+                src={`https://img.spoonacular.com/ingredients_100x100/${ingredient.image}`}
+                className='w-10 h-10 rounded-full'
+                resizeMode='cover'
+              />
+              <View className='flex-row items-center justify-between flex-1'>
+                <Text className='text-lg capitalize'>
+                  {ingredient.nameClean}
+                </Text>
+                <Text className='text-lg'>
+                  {ingredient.amount} {ingredient.measures.us.unitShort}
+                </Text>
+              </View>
+            </View>
+          ))}
         </View>
       </View>
     </ScrollView>
