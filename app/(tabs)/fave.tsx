6f6fcc5120dbc11c9a8718235bc255e334e3cd73
Karl George <karlgeorge87@gmail.com>
2024-09-09 12:30:38 +0200

    Add overlay for number of recipes

--- app/(tabs)/fave.tsx
+++ app/(tabs)/fave.tsx
@@ -10,10 +10,15 @@ import React from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Fave = () => {
-  const { top } = useSafeAreaInsets();
+  const { top, bottom } = useSafeAreaInsets();
+
+  let recipeNumber = 10;
 
   return (
-    <ScrollView className='px-4 bg-bgFaded' style={{ paddingTop: top + 24 }}>
+    <ScrollView
+      className='h-full px-4 bg-bgFaded'
+      style={{ paddingTop: top + 24, paddingBottom: 48 }}
+    >
       <StatusBar backgroundColor={'#F6F6F6'} />
       <View className='mb-2'>
         <Text className='text-2xl font-bold'>Faves</Text>
@@ -43,34 +48,108 @@ const Fave = () => {
               resizeMode='cover'
             />
           </View>
-          <View className='w-full overflow-hidden rounded-md h-3/6'>
+          <View className='w-full overflow-hidden rounded-md h-3/6 '>
             <ImageBackground
               src='https://img.spoonacular.com/recipes/660368-312x231.jpg'
               className='w-full h-full'
               resizeMode='cover'
-            />
+            >
+              <View className='w-full h-full bg-black opacity-50' />
+            </ImageBackground>
+            <View className='absolute w-full h-full'>
+              <Text className='pt-4 text-lg text-center text-white'>
+                {recipeNumber}+
+              </Text>
+              <Text className='text-lg text-center text-white'>Recipes</Text>
+            </View>
           </View>
         </View>
       </View>
 
-      {/* https://img.spoonacular.com/recipes/756814-312x231.jpg */}
-      {/* https://img.spoonacular.com/recipes/655235-312x231.jpg */}
-      {/* https://img.spoonacular.com/recipes/660368-312x231.jpg */}
-
       {/* Lunch */}
-      <View className='flex-row items-center justify-between mb-2'>
+      <View className='flex-row items-center justify-between mt-6 mb-2'>
         <Text className='text-lg font-bold'>Lunch</Text>
         <TouchableOpacity onPress={() => {}}>
           <Text className='font-semibold text-accent'>See all</Text>
         </TouchableOpacity>
       </View>
 
+      <View className='flex-row space-x-2 overflow-hidden'>
+        <View className='w-3/6 h-48 overflow-hidden rounded-md'>
+          <ImageBackground
+            src='https://img.spoonacular.com/recipes/756814-312x231.jpg'
+            className='w-full h-full'
+            resizeMode='cover'
+          />
+        </View>
+        <View className='flex-col h-48 space-y-2 overflow-hidden rounded-md w-[182px]'>
+          <View className='w-full overflow-hidden rounded-md h-3/6'>
+            <ImageBackground
+              src='https://img.spoonacular.com/recipes/655235-312x231.jpg'
+              className='w-full h-full'
+              resizeMode='cover'
+            />
+          </View>
+          <View className='w-full overflow-hidden rounded-md h-3/6 '>
+            <ImageBackground
+              src='https://img.spoonacular.com/recipes/660368-312x231.jpg'
+              className='w-full h-full'
+              resizeMode='cover'
+            >
+              <View className='w-full h-full bg-black opacity-50' />
+            </ImageBackground>
+            <View className='absolute w-full h-full'>
+              <Text className='pt-4 text-lg text-center text-white'>
+                {recipeNumber}+
+              </Text>
+              <Text className='text-lg text-center text-white'>Recipes</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
       {/* Dinner */}
-      <View className='flex-row items-center justify-between mb-2'>
-        <Text className='text-lg font-bold'>Dinner</Text>
-        <TouchableOpacity onPress={() => {}}>
-          <Text className='font-semibold text-accent'>See all</Text>
-        </TouchableOpacity>
+      <View className='pb-20 mb-20'>
+        <View className='flex-row items-center justify-between mt-6 mb-2'>
+          <Text className='text-lg font-bold'>Dinner</Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text className='font-semibold text-accent'>See all</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className='flex-row space-x-2 overflow-hidden'>
+          <View className='w-3/6 h-48 overflow-hidden rounded-md'>
+            <ImageBackground
+              src='https://img.spoonacular.com/recipes/756814-312x231.jpg'
+              className='w-full h-full'
+              resizeMode='cover'
+            />
+          </View>
+          <View className='flex-col h-48 space-y-2 overflow-hidden rounded-md w-[182px]'>
+            <View className='w-full overflow-hidden rounded-md h-3/6'>
+              <ImageBackground
+                src='https://img.spoonacular.com/recipes/655235-312x231.jpg'
+                className='w-full h-full'
+                resizeMode='cover'
+              />
+            </View>
+            <View className='w-full overflow-hidden rounded-md h-3/6 '>
+              <ImageBackground
+                src='https://img.spoonacular.com/recipes/660368-312x231.jpg'
+                className='w-full h-full'
+                resizeMode='cover'
+              >
+                <View className='w-full h-full bg-black opacity-50' />
+              </ImageBackground>
+              <View className='absolute w-full h-full'>
+                <Text className='pt-4 text-lg text-center text-white'>
+                  {recipeNumber}+
+                </Text>
+                <Text className='text-lg text-center text-white'>Recipes</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
