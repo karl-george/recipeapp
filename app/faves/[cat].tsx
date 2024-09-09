1d0ff6982090e5e996edb61dc038499e6bffbbf2
Karl George <karlgeorge87@gmail.com>
2024-09-09 12:55:23 +0200

    Add food grid

--- app/faves/[cat].tsx
+++ app/faves/[cat].tsx
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar, FlatList, ScrollView } from 'react-native';
 import React from 'react';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { data } from '@/constants/data';
+import FoodCard from '@/components/FoodCard';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const FaveCategory = () => {
+  const { cat } = useLocalSearchParams();
+  const { top, bottom } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>FaveCategory</Text>
-    </View>
+    <ScrollView
+      className='px-4 pt-4 bg-bgFaded'
+      showsHorizontalScrollIndicator={false}
+    >
+      <Stack.Screen
+        options={{
+          headerTitle: `${cat}`,
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: '#F6F6F6' },
+          headerShadowVisible: false,
+        }}
+      />
+      <StatusBar backgroundColor={'#F6F6F6'} />
+
+      <FlatList
+        data={data?.results}
+        contentContainerStyle={{ marginBottom: bottom + 24 }}
+        numColumns={2}
+        columnWrapperStyle={{ justifyContent: 'space-between' }}
+        scrollEnabled={false}
+        renderItem={({ item }) => (
+          <FoodCard
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            readyInMinutes={item.readyInMinutes}
+            likes={item.aggregateLikes}
+          />
+        )}
+      />
+    </ScrollView>
   );
 };
 
