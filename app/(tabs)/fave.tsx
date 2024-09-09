c14452e743872bb1d5611ff2ccc651d4d6279977
Karl George <karlgeorge87@gmail.com>
2024-09-09 12:12:22 +0200

    Style image grid

--- app/(tabs)/fave.tsx
+++ app/(tabs)/fave.tsx
@@ -4,6 +4,7 @@ import {
   ScrollView,
   StatusBar,
   TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 import React from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -25,6 +26,36 @@ const Fave = () => {
           <Text className='font-semibold text-accent'>See all</Text>
         </TouchableOpacity>
       </View>
+
+      <View className='flex-row space-x-2 overflow-hidden'>
+        <View className='w-3/6 h-48 overflow-hidden rounded-md'>
+          <ImageBackground
+            src='https://img.spoonacular.com/recipes/756814-312x231.jpg'
+            className='w-full h-full'
+            resizeMode='cover'
+          />
+        </View>
+        <View className='flex-col h-48 space-y-2 overflow-hidden rounded-md w-[182px]'>
+          <View className='w-full overflow-hidden rounded-md h-3/6'>
+            <ImageBackground
+              src='https://img.spoonacular.com/recipes/655235-312x231.jpg'
+              className='w-full h-full'
+              resizeMode='cover'
+            />
+          </View>
+          <View className='w-full overflow-hidden rounded-md h-3/6'>
+            <ImageBackground
+              src='https://img.spoonacular.com/recipes/660368-312x231.jpg'
+              className='w-full h-full'
+              resizeMode='cover'
+            />
+          </View>
+        </View>
+      </View>
+
+      {/* https://img.spoonacular.com/recipes/756814-312x231.jpg */}
+      {/* https://img.spoonacular.com/recipes/655235-312x231.jpg */}
+      {/* https://img.spoonacular.com/recipes/660368-312x231.jpg */}
 
       {/* Lunch */}
       <View className='flex-row items-center justify-between mb-2'>
