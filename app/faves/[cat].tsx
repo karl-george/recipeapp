5401d752add524e98c2bbbd03c21068f7a1702da
Karl George <karlgeorge87@gmail.com>
2024-09-09 12:39:32 +0200

    Add recipe cat page for faves

---
+++ app/faves/[cat].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const FaveCategory = () => {
+  return (
+    <View>
+      <Text>FaveCategory</Text>
+    </View>
+  );
+};
+
+export default FaveCategory;
