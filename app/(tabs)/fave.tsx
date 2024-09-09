4709c9b9c6b5f439c89d1c0e5a94548f3c6b91d4
Karl George <karlgeorge87@gmail.com>
2024-09-09 10:29:56 +0200

    Add titles

--- app/(tabs)/fave.tsx
+++ app/(tabs)/fave.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Fave = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View className='items-center justify-center flex-1'>
-      <Text>Fave</Text>
-    </View>
+    <ScrollView className='px-4 bg-bgFaded' style={{ paddingTop: top + 24 }}>
+      <StatusBar backgroundColor={'#F6F6F6'} />
+      <View className='mb-2'>
+        <Text className='text-2xl font-bold'>Faves</Text>
+      </View>
+
+      {/* Breakfast */}
+      <View className='flex-row items-center justify-between mb-2'>
+        <Text className='text-lg font-bold'>Breakfast</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <Text className='font-semibold text-accent'>See all</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Lunch */}
+      <View className='flex-row items-center justify-between mb-2'>
+        <Text className='text-lg font-bold'>Lunch</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <Text className='font-semibold text-accent'>See all</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Dinner */}
+      <View className='flex-row items-center justify-between mb-2'>
+        <Text className='text-lg font-bold'>Dinner</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <Text className='font-semibold text-accent'>See all</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
