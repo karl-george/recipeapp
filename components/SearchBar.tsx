45d7bf6809d695161455ae62a744d396c910a2b9
Karl George <karlgeorge87@gmail.com>
2024-09-09 13:30:19 +0200

    Add routing

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,37 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
-import { TextInput, TouchableOpacity, View } from 'react-native';
+import { usePathname, useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert, TextInput, TouchableOpacity, View } from 'react-native';
 
 const SearchBar = () => {
+  const [query, setQuery] = useState('');
+
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <View className='flex-row items-center w-full h-10 p-2 mb-6 space-x-4 bg-white border rounded-md border-faded'>
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert('No search term, Please enter a search term');
+          }
+
+          if (pathname.startsWith('/search')) {
+            router.setParams({ query });
+          } else {
+            router.push(`/search/${query}`);
+          }
+        }}
+      >
         <Ionicons name='search' size={20} color={'#AEAAAA'} />
       </TouchableOpacity>
       <TextInput
         className='flex-1'
         placeholder='Search for recipes...'
         placeholderTextColor={'#AEAAAA'}
+        value={query}
+        onChangeText={(e) => setQuery(e)}
       />
       {/* //Todo: Add a cross icon that deletes the entered text to the right side */}
     </View>
