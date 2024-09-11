033df79334a70c5acacef3165a6abe7bf1e57ca6
Karl George <karlgeorge87@gmail.com>
2024-09-11 11:12:03 +0200

    Add using api data

--- app/recipe/[id].tsx
+++ app/recipe/[id].tsx
@@ -1,9 +1,9 @@
-import { detailedRecipe } from '@/constants/data';
+import { Recipe } from '@/types';
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Image,
   ImageBackground,
@@ -14,18 +14,31 @@ import {
 } from 'react-native';
 
 const RecipeDetails = () => {
+  const [recipe, setRecipe] = useState<Recipe>();
   const [fave, setFave] = useState(true);
 
   const { id } = useLocalSearchParams();
-
   const router = useRouter();
+
+  const fetchRecipe = async () => {
+    const res = await fetch(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.apiKey}&includeNutrition=true`
+    );
+
+    const data = await res.json();
+    setRecipe(data);
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+  }, [id]);
 
   return (
     <ScrollView className='flex-1 w-full'>
       <StatusBar translucent />
       <View>
         <ImageBackground
-          src={detailedRecipe?.image}
+          src={recipe?.image}
           className='w-full h-[300px]'
           resizeMode='cover'
         >
@@ -52,25 +65,23 @@ const RecipeDetails = () => {
       {/* Info */}
       <View className='px-4 mt-4'>
         {/* Title */}
-        <Text className='text-2xl font-bold'>{detailedRecipe?.title}</Text>
+        <Text className='text-2xl font-bold'>{recipe?.title}</Text>
         <View className='flex-row space-x-4'>
           <View className='flex-row items-center space-x-1'>
             <Ionicons name='time-outline' size={14} color={'#EA7A22'} />
             <Text className='text-sm text-faded'>
-              {detailedRecipe?.readyInMinutes} min
+              {recipe?.readyInMinutes} min
             </Text>
           </View>
           <View className='flex-row items-center space-x-1'>
             <Ionicons name='flame' size={14} color={'#EA7A22'} />
             <Text className='text-sm text-faded'>
-              {detailedRecipe?.nutrition.nutrients[0].amount.toFixed(0)} cal
+              {recipe?.nutrition.nutrients[0].amount.toFixed(0)} cal
             </Text>
           </View>
           <View className='flex-row items-center space-x-1'>
             <Ionicons name='people' size={14} color={'#EA7A22'} />
-            <Text className='text-sm text-faded'>
-              {detailedRecipe?.servings}
-            </Text>
+            <Text className='text-sm text-faded'>{recipe?.servings}</Text>
           </View>
         </View>
 
@@ -79,7 +90,7 @@ const RecipeDetails = () => {
           showsHorizontalScrollIndicator={false}
           className='flex-row space-x-2'
         >
-          {detailedRecipe.diets.map((diet, i) => (
+          {recipe?.diets.map((diet, i) => (
             <View key={i} className='flex-row mt-4'>
               <LinearGradient
                 start={{ x: 0, y: 0 }}
@@ -98,7 +109,7 @@ const RecipeDetails = () => {
         {/* Ingredients */}
         <View className='mt-6'>
           <Text className='mb-3 text-xl font-bold'>Ingredients</Text>
-          {detailedRecipe?.extendedIngredients.map((ingredient, i) => (
+          {recipe?.extendedIngredients.map((ingredient, i) => (
             <View className='flex-row items-center mb-4 space-x-2' key={i}>
               <Image
                 src={`https://img.spoonacular.com/ingredients_100x100/${ingredient.image}`}
@@ -120,7 +131,7 @@ const RecipeDetails = () => {
         {/* Instructions */}
         <View className='mt-4'>
           <Text className='mb-3 text-xl font-bold'>Instructions</Text>
-          {detailedRecipe?.analyzedInstructions[0].steps.map((step, i) => (
+          {recipe?.analyzedInstructions[0].steps.map((step, i) => (
             <View className='flex-row mb-4 space-x-2' key={i}>
               <Text className='text-lg'>{step.number}.</Text>
               <Text className='w-11/12 text-lg'>{step.step}</Text>
@@ -133,65 +144,65 @@ const RecipeDetails = () => {
           <Text className='mb-3 text-xl font-bold'>Nutrition Per Serving</Text>
           <View className='flex-row flex-1 space-x-2'>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[0].name}:
+              {recipe?.nutrition.nutrients[0].name}:
             </Text>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[0].amount.toFixed(0)}
-              {detailedRecipe?.nutrition.nutrients[0].unit}
-            </Text>
-          </View>
-          <View className='flex-row flex-1 space-x-2'>
-            <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[1].name}:
-            </Text>
-            <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[1].amount.toFixed(0)}
-              {detailedRecipe?.nutrition.nutrients[1].unit}
+              {recipe?.nutrition.nutrients[0].amount.toFixed(0)}
+              {recipe?.nutrition.nutrients[0].unit}
             </Text>
           </View>
           <View className='flex-row flex-1 space-x-2'>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[3].name}:
+              {recipe?.nutrition.nutrients[1].name}:
             </Text>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[3].amount.toFixed(0)}
-              {detailedRecipe?.nutrition.nutrients[3].unit}
-            </Text>
-          </View>
-          <View className='flex-row flex-1 space-x-2'>
-            <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[5].name}:
-            </Text>
-            <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[5].amount.toFixed(0)}
-              {detailedRecipe?.nutrition.nutrients[5].unit}
+              {recipe?.nutrition.nutrients[1].amount.toFixed(0)}
+              {recipe?.nutrition.nutrients[1].unit}
             </Text>
           </View>
           <View className='flex-row flex-1 space-x-2'>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[6].name}:
+              {recipe?.nutrition.nutrients[3].name}:
             </Text>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[6].amount.toFixed(0)}
-              {detailedRecipe?.nutrition.nutrients[6].unit}
+              {recipe?.nutrition.nutrients[3].amount.toFixed(0)}
+              {recipe?.nutrition.nutrients[3].unit}
             </Text>
           </View>
           <View className='flex-row flex-1 space-x-2'>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[7].name}:
+              {recipe?.nutrition.nutrients[5].name}:
             </Text>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[7].amount.toFixed(0)}
-              {detailedRecipe?.nutrition.nutrients[7].unit}
+              {recipe?.nutrition.nutrients[5].amount.toFixed(0)}
+              {recipe?.nutrition.nutrients[5].unit}
+            </Text>
+          </View>
+          <View className='flex-row flex-1 space-x-2'>
+            <Text className='text-lg'>
+              {recipe?.nutrition.nutrients[6].name}:
+            </Text>
+            <Text className='text-lg'>
+              {recipe?.nutrition.nutrients[6].amount.toFixed(0)}
+              {recipe?.nutrition.nutrients[6].unit}
+            </Text>
+          </View>
+          <View className='flex-row flex-1 space-x-2'>
+            <Text className='text-lg'>
+              {recipe?.nutrition.nutrients[7].name}:
+            </Text>
+            <Text className='text-lg'>
+              {recipe?.nutrition.nutrients[7].amount.toFixed(0)}
+              {recipe?.nutrition.nutrients[7].unit}
             </Text>
           </View>
           <View className='flex-row flex-1 mb-3 space-x-2'>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[10].name}:
+              {recipe?.nutrition.nutrients[10].name}:
             </Text>
             <Text className='text-lg'>
-              {detailedRecipe?.nutrition.nutrients[10].amount.toFixed(0)}
-              {detailedRecipe?.nutrition.nutrients[10].unit}
+              {recipe?.nutrition.nutrients[10].amount.toFixed(0)}
+              {recipe?.nutrition.nutrients[10].unit}
             </Text>
           </View>
         </View>
