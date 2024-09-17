310add46630eea28f4a3aae6df323497d8c8659f
Karl George <karlgeorge87@gmail.com>
2024-09-17 15:57:13 +0200

    Add image

--- app/index.tsx
+++ app/index.tsx
@@ -6,21 +6,27 @@ export default function Index() {
   const router = useRouter();
 
   return (
-    <View>
-      <Image src='/app/assets/images/logo.png' width={196} height={46} />
-      <View className='items-center justify-center w-[196px] h-[46px]'>
-        <TouchableOpacity onPress={() => router.push('/(tabs)/home')}>
-          <LinearGradient
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 0 }}
-            colors={['#EA7A22', '#F5A150']}
-            className='items-center px-5 py-3 rounded-md'
-          >
-            <Text className='text-2xl font-bold text-white capitalize bg-transparent'>
-              Get Started
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
+    <View className='bg-[#DDEFE3] w-full h-full'>
+      <View className='w-full h-full'>
+        <Image
+          source={require('@/assets/images/start-img.jpg')}
+          resizeMode='cover'
+          className='w-full h-4/6'
+        />
+        <View className='items-center justify-center'>
+          <TouchableOpacity onPress={() => router.push('/(tabs)/home')}>
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              colors={['#EA7A22', '#F5A150']}
+              className='items-center px-12 py-3 rounded-md'
+            >
+              <Text className='text-2xl font-bold text-white capitalize bg-transparent'>
+                Get Started
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
