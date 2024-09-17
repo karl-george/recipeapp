6d97cd686ab5ba54bc5655c81a79ab6c211d8489
Karl George <karlgeorge87@gmail.com>
2024-09-17 10:34:23 +0200

    Add retrieving api data

--- app/category/[cat].tsx
+++ app/category/[cat].tsx
@@ -1,13 +1,40 @@
 import FoodCard from '@/components/FoodCard';
-import { data } from '@/constants/data';
+import { Recipes } from '@/types';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { FlatList, ScrollView, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const CategoryList = () => {
+  const [recipes, setRecipes] = useState<Recipes>();
+  const [loading, setLoading] = useState(false);
+
   const { cat } = useLocalSearchParams();
   const { top, bottom } = useSafeAreaInsets();
+
+  const fetchRecipe = async () => {
+    try {
+      const res = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.apiKey}&type=${cat}&instructionsRequired=true&addRecipeInformation=true`
+      );
+
+      const data = await res.json();
+      setRecipes(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+  }, [cat]);
 
   return (
     <ScrollView
@@ -23,9 +50,10 @@ const CategoryList = () => {
         }}
       />
       <StatusBar backgroundColor={'#F6F6F6'} />
+      {loading && <ActivityIndicator size='large' color='#000' />}
 
       <FlatList
-        data={data?.results}
+        data={recipes?.results}
         contentContainerStyle={{ marginBottom: bottom + 24 }}
         numColumns={2}
         columnWrapperStyle={{ justifyContent: 'space-between' }}
