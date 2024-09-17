9d1745cac5fcce065a5ff29e281a186577a02eef
Karl George <karlgeorge87@gmail.com>
2024-09-17 10:07:56 +0200

    Add category see all page

---
+++ app/category/[cat].tsx
@@ -0,0 +1,47 @@
+import FoodCard from '@/components/FoodCard';
+import { data } from '@/constants/data';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import React from 'react';
+import { FlatList, ScrollView, StatusBar } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const CategoryList = () => {
+  const { cat } = useLocalSearchParams();
+  const { top, bottom } = useSafeAreaInsets();
+
+  return (
+    <ScrollView
+      className='px-4 pt-4 bg-bgFaded'
+      showsHorizontalScrollIndicator={false}
+    >
+      <Stack.Screen
+        options={{
+          headerTitle: `${cat}`,
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: '#F6F6F6' },
+          headerShadowVisible: false,
+        }}
+      />
+      <StatusBar backgroundColor={'#F6F6F6'} />
+
+      <FlatList
+        data={data?.results}
+        contentContainerStyle={{ marginBottom: bottom + 24 }}
+        numColumns={2}
+        columnWrapperStyle={{ justifyContent: 'space-between' }}
+        scrollEnabled={false}
+        renderItem={({ item }) => (
+          <FoodCard
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            readyInMinutes={item.readyInMinutes}
+            likes={item.aggregateLikes}
+          />
+        )}
+      />
+    </ScrollView>
+  );
+};
+
+export default CategoryList;
